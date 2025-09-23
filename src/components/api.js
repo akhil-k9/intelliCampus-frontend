e527137a607184fea3117633d46f72097ea6f7cc
kandyula akhil <akhilkandayala@gmail.com>
2025-09-24 00:17:33 +0530

    3 commit

--- src/components/api.js
+++ src/components/api.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000";
+// export const API_URL = "http://localhost:5000";
 
-// export const API_URL = "https://intellicampus-backend-u8b8.onrender.com";
+export const API_URL = "https://intellicampus-backend-u8b8.onrender.com";
